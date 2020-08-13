ea2eeb237ac3388507aa1e9bd978c205900e1762
Tony <t206373@dac.unicamp.br>
2020-08-13 20:35:09 -0300

    codigo funcional

--- src/main.c
+++ src/main.c
@@ -1,23 +1,40 @@
-/* Tarefa de decifrar uma cifra
- *
- * Dica: veja em referencia/cifrador.c o codigo-fonte do programa
- * que gerou as cifras que voce precisa decifrar!
+/* Data de submissao:13/08/2020
+ * Nome: Tony Tian Rui Li
+ * RA:206373
  */
 
 #include <stdio.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 100
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
 
+const char senha[] = "SENHASECRETA";
+
+/*FUNÇÃO PARA DECIFRAR A ENTRADA CRIPTOGRAFADA*/
+char decryption(char a, char b) {
+  char c;
+  if (a>='A' && a<='Z') {
+    c = 'A'+('Z'-'A'+1+a-b)%('Z'-'A'+1);
+    return c;
+  } else return a;
+}
+/* CODIGO TOTALMENTE APROVEITADO DA REFERENCIA */
 int main() {
-  /* Leitura da entrada */
+  int j=0;
+  int i=0;
+ 
   fgets(buffer_entrada, TAM_BUFFER, stdin);
 
+  for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++) {
 
+    buffer_saida[i] = decryption(buffer_entrada[i], senha[j]);
+    j++;
+    if (senha[j]=='\0') j=0;
+  }
+  buffer_saida[i]='\0';
 
-  /* Escrever saida na tela */
   printf("%s\n", buffer_saida);
-
+  
   return 0;
 }
